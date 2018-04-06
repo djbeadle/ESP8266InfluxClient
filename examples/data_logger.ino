4a02db03b0c0b3281972b43aaf5501908f308e0d
djbeadle <djbeadle+github@gmail.com>
2018-04-06 01:27:17 -0400

    Example overhual.

--- examples/data_logger.ino
+++ examples/data_logger.ino
@@ -1,36 +1,51 @@
-/********************************
- *
- * ESP8266 to Influx Server
- *     Library Example
- * 
- * Written by Daniel Beadle in 2018
- ********************************/
+/*
+ ESP8266 Influx Client Library
+ By: Daniel Beadle
+ Date: April 6th, 2018
+ 
+ This is an ESP8266 library for easily logging data to an InfluxDB server!
+ 
+ This Library is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This Library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ For a copy of the GNU General Public License, see
+ <http://www.gnu.org/licenses/>.
+ */
 
-#include <ESP8266InfluxServer.h>
-
+// Libraries required for networking on the ESP8266
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+
+// This library, woo!
+#include <ESP8266InfluxClient.h>
 
 // For the Si7021 temperature sensor:
 #include "SparkFun_Si7021_Breakout_Library.h"
 
-/**  
- * InfluxDB Server Settings
- */
-const char * host = "192.168.1.152";  // ip or domain name
-uint16_t port = 8086;
+/**********************
+ * USER SETTINGS HERE *
+ **********************/
+// Set your Wi-Fi details here
+const char *ssid ="Linksys";
+const char *password ="SuperFastInternet42";
 
-/**
- * Measurement Settings
- * 
- * Stored in a struct. If you're logging multiple measurements,
- * create multiple Measurement structs.
- */
-Measurement basement_fahren = {
-  "FiveOhTwo",  // Database name
-  "basement",   // Measurement name
+// Set the details of your InfluxDB server here
+const char * host = "192.168.1.143";  // ip or domain name
+uint16_t port = 8086; // Set the port your server is listening for data on. Default is 8086
+
+// Measurement Settings
+// These are stored in a struct. If you're logging multiple measurements,
+// create multiple structs.
+Measurement bedroom_temp = {
+  "my_house_data",  // Database name
+  "bedroom",   // Measurement name
   "fahrenheit",      // Field key
-  "esp8266-basement2", // Host
+  "esp8266-bedroom-1", // Host name (A name or unique id for the sensor)
   "us-east"     // Region
 };
 
@@ -41,36 +56,21 @@ Weather sensor;
 ESP8266WiFiMulti WiFiMulti;
 
 // Create the database object
-ESP8266InfluxServer db_net_server = ESP8266InfluxServer(host, port);
+ESP8266InfluxClient influx_server = ESP8266InfluxClient(host, port);
 
 float read_temperature()
 {
-  float humidity = 0;
-  float tempf = 0;
-  
-  // Measure Relative Humidity from the HTU21D or Si7021
-  humidity = sensor.getRH();
-
   // Measure Temperature from the HTU21D or Si7021
-  // Temperature is measured every time RH is requested.
-  // It is faster, therefore, to read it from previous RH
-  // measurement with getTemp() instead with readTemp()
+  float tempf = 0;
   tempf = sensor.getTempF();
   
   Serial.print(tempf);            // print the Fahrenheit
   Serial.println(" degrees Fahrenheit,");
 
-  Serial.print( (tempf - 32) * 5 / 9 );  //  convert to Celsius
-  Serial.println(" degrees Celsius, ");
-
-  Serial.print(humidity/100);
-  Serial.println(" relative humidity,");
-
   return tempf;
 }
 
 void setup() {
-  delay(500);
   Serial.begin(115200);
   Serial.println("ESP8266-01 Online");
 
@@ -92,8 +92,10 @@ void setup() {
 }
 
 void loop() {
-  db_net_server.update(basement_fahren, read_temperature());
+  // Update the server!
+  // If the value returned is >= 0, then it was a success!
+  influx_server.update(bedroom_temp, read_temperature());
 
   // Pause for 60 seconds
-  delay(5000);
+  delay(60000);
 }
